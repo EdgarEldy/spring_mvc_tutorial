dc6280fd0e64abc67ab863627ec2940602add029
Edgar Eldy <ed.eldy21@live.com>
2021-01-30 12:03:10 +0200

    initial commit

---
+++ src/test/java/com/thefreedevelopers/spring_mvc_tutorial/SpringMvcTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefreedevelopers.spring_mvc_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
