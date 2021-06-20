b0ecaaef614727dfe78a57690e821132a53f04e2
Edgar Eldy <ed.eldy21@live.com>
2021-06-20 21:55:53 +0200

    Initialize CategoryService

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CategoryController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CategoryController.java
@@ -1,7 +1,14 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CategoryController {
+
+    //Initialize CategoryService
+    @Autowired
+    private CategoryService service;
+
 }
