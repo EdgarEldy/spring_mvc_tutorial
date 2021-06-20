808d1c407c4bf99121361ba0c233f6d819636fa8
Edgar Eldy <ed.eldy21@live.com>
2021-06-20 21:27:01 +0200

    Initialize CategoryRepository

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CategoryService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CategoryService.java
@@ -1,7 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    //Initialize CategoryRepository
+    private CategoryRepository repository;
 }
