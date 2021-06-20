4a957a123a416126b2e45a316a1ca2bd082de324
Edgar Eldy <ed.eldy21@live.com>
2021-06-20 21:19:35 +0200

    Name Category Entity to categories table

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Category.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Category.java
@@ -1,11 +1,9 @@
 package com.thefreedevelopers.spring_mvc_tutorial.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
