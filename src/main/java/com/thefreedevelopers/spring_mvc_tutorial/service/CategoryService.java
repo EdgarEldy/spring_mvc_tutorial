ce533e46f643b7a34e860c1dff06c9848b323a3c
Edgar Eldy <ed.eldy21@live.com>
2021-06-20 21:29:35 +0200

    Add a method for getting all categories

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CategoryService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Category;
 import com.thefreedevelopers.spring_mvc_tutorial.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -10,4 +13,9 @@ public class CategoryService {
     @Autowired
     //Initialize CategoryRepository
     private CategoryRepository repository;
+
+    //Get all categories
+    public List<Category> getCategories(){
+        return repository.findAll();
+    }
 }
