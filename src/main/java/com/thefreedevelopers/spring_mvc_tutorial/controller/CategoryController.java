712e855f2d26216551b83605b86d947c7f607f42
Edgar Eldy <ed.eldy21@live.com>
2021-06-23 21:14:12 +0200

    Add editPage method for getting categories/edit view with data

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CategoryController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -36,5 +37,13 @@ public class CategoryController {
     public String savePage(@ModelAttribute("category") Category category) {
         service.saveCategory(category);
         return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/edit/{id}")
+    public String editPage(@PathVariable(value = "id") long id, Model model) {
+    //Get category from CategoryService
+        Category category = service.getCategoryById(id);
+        model.addAttribute("category", category);
+        return "categories/edit";
     }
 }
