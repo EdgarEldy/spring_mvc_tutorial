5e4dd329769ed35e3a29a379ca11c62747cead20
Edgar Eldy <ed.eldy21@live.com>
2021-10-03 20:39:40 +0200

    Initialize ProductService

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
@@ -1,7 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProductController {
+
+    // Initialize ProductService
+    @Autowired
+    private ProductService productService;
 }
