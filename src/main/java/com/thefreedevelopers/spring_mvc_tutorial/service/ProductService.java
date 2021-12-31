e984edb3201d534458ecc6dcb8198c6f1b7c259d
Edgar Eldy <ed.eldy21@live.com>
2021-12-31 12:03:18 +0200

    Get all products

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/ProductService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/ProductService.java
@@ -1,12 +1,20 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Product;
 import com.thefreedevelopers.spring_mvc_tutorial.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     //Initialize ProductRepository
     private ProductRepository repository;
+
+    // Get all products
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
 }
