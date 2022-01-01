5892dcf256489854d79ce84a885966020236727b
Edgar Eldy <ed.eldy21@live.com>
2022-01-01 11:44:19 +0200

    Add function for getting products

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Product;
 import com.thefreedevelopers.spring_mvc_tutorial.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -10,4 +15,12 @@ public class ProductController {
     // Initialize ProductService
     @Autowired
     private ProductService productService;
+
+    // Show products api
+    @GetMapping("/products")
+    public String indexPage(Model model){
+        List<Product> products = productService.getProducts();
+        model.addAttribute("products", products);
+        return "products/index";
+    }
 }
