62f2a61a57e1d3aa46a3f0b443b0d843ef6f5d7e
Edgar Eldy <ed.eldy21@live.com>
2022-01-05 23:33:46 +0200

    Add products post route for saving a new product

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -38,5 +40,11 @@ public class ProductController {
         Product product = new Product();
         model.addAttribute("product", product);
         return "products/add";
+    }
+
+    @PostMapping("/products")
+    public String savePage(@ModelAttribute("product") Product product) {
+        productService.saveProduct(product);
+        return "redirect:/products";
     }
 }
