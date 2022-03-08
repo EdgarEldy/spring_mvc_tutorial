3d81e76615f8b3653dc40d42a0a4f380a0bd41e5
Edgar Eldy <ed.eldy21@live.com>
2022-03-08 22:30:51 +0200

    Get a customer using id

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CustomerService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CustomerService.java
@@ -1,11 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Category;
 import com.thefreedevelopers.spring_mvc_tutorial.entity.Customer;
 import com.thefreedevelopers.spring_mvc_tutorial.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -26,5 +28,17 @@ public class CustomerService {
     // Save a customer
     public void saveCustomer(Customer customer) {
         customerRepository.save(customer);
+    }
+
+    //Get a customer using id
+    public Customer getCustomerById(long id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        Customer customer = null;
+        if (customerOptional.isPresent()) {
+            customer = customerOptional.get();
+        } else {
+            throw new RuntimeException("Customer not found:" + id);
+        }
+        return customer;
     }
 }
