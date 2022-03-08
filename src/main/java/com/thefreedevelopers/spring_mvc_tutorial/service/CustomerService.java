529c655a07b0a7b7e1d45db83b90e0a331677e42
Edgar Eldy <ed.eldy21@live.com>
2022-03-08 22:24:26 +0200

    Getting all customers

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CustomerService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/CustomerService.java
@@ -1,11 +1,25 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Customer;
+import com.thefreedevelopers.spring_mvc_tutorial.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
 
     // Constructor
     public CustomerService() {
+    }
+
+    // Initialize CustomerRepository
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // Getting all customers
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 }
