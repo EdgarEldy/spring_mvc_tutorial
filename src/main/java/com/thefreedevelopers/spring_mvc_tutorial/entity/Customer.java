466b0f47ea3fd7903f5fec53183ceca46b5575f7
Edgar Eldy <ed.eldy21@live.com>
2022-03-08 15:22:11 +0200

    Generate properties

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Customer.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Customer.java
@@ -21,4 +21,51 @@ public class Customer {
     // Constructor
     public Customer() {
     }
+
+    // Properties
+    private String firstName;
+    private String lastName;
+    private String Tel;
+    private String Email;
+    private String Address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String tel) {
+        Tel = tel;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
 }
