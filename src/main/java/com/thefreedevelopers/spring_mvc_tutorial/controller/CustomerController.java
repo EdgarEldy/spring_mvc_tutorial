ee79b26b914b18b864d31b31a3c2aeb8d37e3350
Edgar Eldy <ed.eldy21@live.com>
2022-03-09 15:08:58 +0200

    Initialize CustomerService

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CustomerController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.service.CustomerService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CustomerController {
+
+    // Initialize CustomerService
+    private CustomerService customerService;
 
     // Constructor
     public CustomerController() {
