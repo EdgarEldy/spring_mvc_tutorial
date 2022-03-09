59428fc4f46d92b77727f6aae07ed8d8a1144e4a
Edgar Eldy <ed.eldy21@live.com>
2022-03-09 15:35:23 +0200

    Show customers function

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CustomerController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Customer;
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Product;
 import com.thefreedevelopers.spring_mvc_tutorial.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
 
     // Initialize CustomerService
+    @Autowired
     private CustomerService customerService;
 
     // Constructor
     public CustomerController() {
+    }
+
+    // Show customers
+    @GetMapping("/customers")
+    public String indexPage(Model model) {
+        List<Customer> customers = customerService.getCustomers();
+        model.addAttribute("customers", customers);
+        return "customers/index";
     }
 }
