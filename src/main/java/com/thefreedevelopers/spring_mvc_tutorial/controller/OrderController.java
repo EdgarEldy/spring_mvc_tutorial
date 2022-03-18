5725ffaf89149232245f073aea46864d5a6514bc
Edgar Eldy <ed.eldy21@live.com>
2022-03-18 12:20:09 +0200

    Initialize OrderService

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/OrderController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/OrderController.java
@@ -1,7 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
+import com.thefreedevelopers.spring_mvc_tutorial.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class OrderController {
+
+    // Initialize OrderService
+    @Autowired
+    private OrderService orderService;
 }
