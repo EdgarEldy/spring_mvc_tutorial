0d5e3ccacf37770b1b43c3b8fcb2865acda9341d
Edgar Eldy <ed.eldy21@live.com>
2022-03-18 11:53:26 +0200

    Initialize CustomerRepository

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/OrderService.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/service/OrderService.java
@@ -1,7 +1,13 @@
 package com.thefreedevelopers.spring_mvc_tutorial.service;
 
+import com.thefreedevelopers.spring_mvc_tutorial.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    // Initialize CustomerRepository
+    @Autowired
+    private CustomerRepository customerRepository;
 }
