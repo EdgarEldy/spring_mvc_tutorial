28567ba1eb6eff2d06f3de82f6859ed2ee20c43d
Edgar Eldy <ed.eldy21@live.com>
2022-03-18 10:45:42 +0200

    Add Order entity

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Order.java
@@ -0,0 +1,20 @@
+package com.thefreedevelopers.spring_mvc_tutorial.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
