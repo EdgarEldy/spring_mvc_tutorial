5bae95d412ac18de2e9f2be23d297c5b78f71b1c
Edgar Eldy <ed.eldy21@live.com>
2022-03-18 12:21:41 +0200

    Initialize ProductService

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/OrderController.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.thefreedevelopers.spring_mvc_tutorial.controller;
 
 import com.thefreedevelopers.spring_mvc_tutorial.service.CustomerService;
 import com.thefreedevelopers.spring_mvc_tutorial.service.OrderService;
+import com.thefreedevelopers.spring_mvc_tutorial.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -15,4 +16,8 @@ public class OrderController {
     // Initialize CustomerService
     @Autowired
     private CustomerService customerService;
+
+    // Initialize ProductService
+    @Autowired
+    private ProductService productService;
 }
