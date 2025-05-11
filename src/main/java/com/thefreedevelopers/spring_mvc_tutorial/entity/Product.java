96cad8c78c393566aa95ac69657c9427f921cf52
EdgarEldy <ed.eldy21@live.com>
2025-05-11 20:01:22 +0200

    Add productName and unitPrice properties

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Product.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Product.java
@@ -9,6 +9,8 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    private String productName;
+    private double unitPrice;
 
     public Long getId() {
         return id;
@@ -18,4 +20,19 @@ public class Product {
         this.id = id;
     }
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
