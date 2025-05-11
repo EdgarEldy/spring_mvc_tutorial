20e8bb7e05ad069c608264b65532a41a9e2f0ce1
EdgarEldy <ed.eldy21@live.com>
2025-05-11 20:10:35 +0200

    Add qty and total properties

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Order.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Order.java
@@ -9,6 +9,8 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    private Integer qty;
+    private Double total;
 
     public Long getId() {
         return id;
@@ -18,4 +20,19 @@ public class Order {
         this.id = id;
     }
 
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
 }
