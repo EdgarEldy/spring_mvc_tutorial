7bf36b229fb68c59f31795268ed0f3457068367f
EdgarEldy <ed.eldy21@live.com>
2025-05-11 20:05:55 +0200

    Add Customer model

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Customer.java
@@ -0,0 +1,21 @@
+package com.thefreedevelopers.spring_mvc_tutorial.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
