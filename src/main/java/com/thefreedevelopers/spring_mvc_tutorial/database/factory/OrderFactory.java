7cc0c45a9466748883aa7c5529cc9bb5a8c9a463
EdgarEldy <ed.eldy21@live.com>
2025-05-12 15:15:12 +0200

    Factory class for generating fake order data using Java Faker

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/OrderFactory.java
@@ -0,0 +1,17 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Factory class for generating fake order data using Java Faker
+ * <p>
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+
+public class OrderFactory {
+    // Shared Faker instance for generating fake data
+    private static final Faker faker = new Faker();
+}
