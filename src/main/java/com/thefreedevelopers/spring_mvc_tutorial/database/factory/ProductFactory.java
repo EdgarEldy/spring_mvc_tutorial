c74b35458e5307361aed4418f3c73ba013911cfe
EdgarEldy <ed.eldy21@live.com>
2025-05-12 11:36:21 +0200

    Factory class for generating fake product data for testing

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/ProductFactory.java
@@ -0,0 +1,12 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
+
+/**
+ * Factory class for generating fake product data for testing
+ *
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+public class ProductFactory {
+}
