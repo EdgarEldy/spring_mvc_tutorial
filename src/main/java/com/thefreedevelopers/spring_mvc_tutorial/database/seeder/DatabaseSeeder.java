2dbce90e481b3fdb145d8a9be2a98cd21ee0a701
EdgarEldy <ed.eldy21@live.com>
2025-05-12 09:41:19 +0200

    Add component class for generating seeders

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/DatabaseSeeder.java
@@ -0,0 +1,22 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.seeder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * Component class for generating seeders
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+
+@Component
+@Profile("dev")
+public class DatabaseSeeder implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
