29846377344f915b73275700ea5964c6893c35cb
EdgarEldy <ed.eldy21@live.com>
2025-05-12 10:57:03 +0200

    Run CategorySeeder

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/DatabaseSeeder.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/DatabaseSeeder.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * Component class for generating seeders
+ *
  * Created by EdgarEldy on 5/12/2025
  * Author : EdgarEldy
  * Date : 5/12/2025
@@ -15,8 +16,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("dev")
 public class DatabaseSeeder implements CommandLineRunner {
+    private final CategorySeeder categorySeeder;
+
+    public DatabaseSeeder(CategorySeeder categorySeeder) {
+        this.categorySeeder = categorySeeder;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        categorySeeder.seed();
     }
 }
