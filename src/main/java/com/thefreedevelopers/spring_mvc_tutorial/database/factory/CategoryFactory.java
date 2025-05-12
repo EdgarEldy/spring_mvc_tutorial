d3833b6f49973447c45b6cf53dea89be335e603c
EdgarEldy <ed.eldy21@live.com>
2025-05-12 09:04:29 +0200

    Add factory class for generating fake category data for testing

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CategoryFactory.java
@@ -0,0 +1,12 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
+
+/**
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+
+// Factory class for generating fake category data for testing
+public class CategoryFactory {
+}
