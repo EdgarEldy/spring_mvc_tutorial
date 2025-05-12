da1dc7f085afa76e6336e498a5253ac4fcbbeeb3
EdgarEldy <ed.eldy21@live.com>
2025-05-12 09:42:49 +0200

    Add a factory class for generating fake category data for testing

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CategoryFactory.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CategoryFactory.java
@@ -1,12 +1,34 @@
 package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
 
+import com.github.javafaker.Faker;
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Factory class for generating fake category data for testing
  * Created by EdgarEldy on 5/12/2025
  * Author : EdgarEldy
  * Date : 5/12/2025
  * Project : spring_mvc_tutorial
  */
 
-// Factory class for generating fake category data for testing
 public class CategoryFactory {
+    private static final Faker faker = new Faker();
+
+    // Create fake category names
+    public static Category create() {
+        return new Category(faker.commerce().department()); // e.g., "Electronics", "Books", "Toys"
+    }
+
+    // Create fake categories based on count parameter: e.g, 10 categories
+    public List<Category> createCategories(int count) {
+        List<Category> categories = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            categories.add(create());
+        }
+
+        return categories;
+    }
 }
