3cb701b7f1ea306b626746b8bab2d6b9d47051a1
EdgarEldy <ed.eldy21@live.com>
2025-05-12 13:47:38 +0200

    Factory class for generating fake customer data using Java Faker

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CustomerFactory.java
@@ -0,0 +1,13 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
+
+/**
+ * Factory class for generating fake customer data using Java Faker
+ * <p>
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+
+public class CustomerFactory {
+}
