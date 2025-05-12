bf031c890fa5ab9e9dd9e08870de633980b1301f
EdgarEldy <ed.eldy21@live.com>
2025-05-12 15:34:43 +0200

    Add a seeder class for generating fake order data based on OrderFactory

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/OrderSeeder.java
@@ -0,0 +1,18 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.seeder;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * Seeder class for generating fake order data based on OrderFactory
+ * <p>
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+
+@Component
+@Profile("dev")
+public class OrderSeeder {
+}
