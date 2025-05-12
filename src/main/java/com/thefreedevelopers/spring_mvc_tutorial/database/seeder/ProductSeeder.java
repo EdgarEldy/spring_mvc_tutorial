abc5b9ce335b7e662f194b3d8f09518ca2dc91de
EdgarEldy <ed.eldy21@live.com>
2025-05-12 12:52:14 +0200

    Seeder class for generating fake product data based on ProductFactory

---
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/ProductSeeder.java
@@ -0,0 +1,12 @@
+package com.thefreedevelopers.spring_mvc_tutorial.database.seeder;
+
+/**
+ * Seeder class for generating fake product data based on ProductFactory
+ * <p>
+ * Created by EdgarEldy on 5/12/2025
+ * Author : EdgarEldy
+ * Date : 5/12/2025
+ * Project : spring_mvc_tutorial
+ */
+public class ProductSeeder {
+}
