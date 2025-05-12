d56b5cc41a1b14fea9d1dfbeae48877a3022e5ca
EdgarEldy <ed.eldy21@live.com>
2025-05-12 14:03:57 +0200

    Constructor with dependency injection for the customerRepository

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/CustomerSeeder.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/seeder/CustomerSeeder.java
@@ -1,5 +1,6 @@
 package com.thefreedevelopers.spring_mvc_tutorial.database.seeder;
 
+import com.thefreedevelopers.spring_mvc_tutorial.repository.CustomerRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -15,4 +16,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("dev")
 public class CustomerSeeder {
+    private final CustomerRepository customerRepository;
+
+    /**
+     * Constructor with dependency injection for the customerRepository
+     *
+     * @param customerRepository repository for accessing Customer data
+     */
+    public CustomerSeeder(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 }
