5fd88c0afe5d63b7a347f4a825ca1af97dbe28c5
EdgarEldy <ed.eldy21@live.com>
2025-05-12 13:53:16 +0200

    Creates a single customer with a random first name, last name, phone number, email and address

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CustomerFactory.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/database/factory/CustomerFactory.java
@@ -1,5 +1,8 @@
 package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
 
+import com.github.javafaker.Faker;
+import com.thefreedevelopers.spring_mvc_tutorial.entity.Customer;
+
 /**
  * Factory class for generating fake customer data using Java Faker
  * <p>
@@ -10,4 +13,21 @@ package com.thefreedevelopers.spring_mvc_tutorial.database.factory;
  */
 
 public class CustomerFactory {
+    // Shared Faker instance for generating fake data
+    private static final Faker faker = new Faker();
+
+    /**
+     * Creates a single customer with a random first name, last name, phone number, email and address
+     *
+     * @return a customer instance with dummy data
+     */
+    public static Customer create() {
+        return new Customer(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.phoneNumber().phoneNumber(),
+                faker.internet().emailAddress(),
+                faker.address().fullAddress()
+        );
+    }
 }
